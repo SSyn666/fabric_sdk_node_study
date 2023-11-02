8626f8bcf3982ee6d5ead42d7781395be8ab991d
xixi.shi <xixi.shi@vinotech.com>
2023-11-02 22:19:30 +0800

    Update mainorigin.js

--- mainorigin.js
+++ mainorigin.js
@@ -17,7 +17,10 @@ app.get('/init',
             var salesterminalinitresult = yield fabricservice.sendTransaction(cow_cc_name, "invoke", ["putvalue", milk_bottle, machiningid]); 
             for (let i = 0; i ＜ chiancodequeryresutl.length; i++) { 
               res.send( dairyfarminitresult[i].toString( 'utf8' ) ); 
-            } }).catch((err) =＞ { res.send(err); }) }); 
+            } 
+          }).catch((err) =＞ { res.send(err); }) 
+          }
+       ); 
 // 奶牛场的相关操作 
 app.get('/dairyfarm', 
         function (req, res) { 
@@ -26,7 +29,10 @@ app.get('/dairyfarm',
             var chiancodequeryresutl = yield fabricservice.sendTransaction(cow_cc_name, "invoke", [ "putvalue" , cowid , parm ]); 
             for (let i = 0; i ＜ chiancodequeryresutl.length; i++) { 
               res.send( chiancodequeryresutl[i].toString( 'utf8' ) ); 
-            } }).catch((err) =＞ { res.send(err); }) }); 
+            } 
+          }).catch((err) =＞ { res.send(err); }) 
+        }
+       ); 
 // 加工车间的操作 
 app.get('/machining', 
         function (req, res) { 
@@ -35,7 +41,10 @@ app.get('/machining',
             var chiancodequeryresutl = yield fabricservice.sendTransaction(machining_cc_name, "invoke", ["putvalue", machiningid, parm]); 
             for (let i = 0; i ＜ chiancodequeryresutl.length; i++) { 
               res.send( chiancodequeryresutl[i].toString( 'utf8' ) );
-            } }).catch((err) =＞ { res.send(err); }) });
+            } 
+          }).catch((err) =＞ { res.send(err); }) 
+        }
+       );
 // 销售终端的操作 
 app.get('/salesterminal', 
         function (req, res) { 
@@ -44,7 +53,9 @@ app.get('/salesterminal',
             var chiancodequeryresutl = yield fabricservice.sendTransaction(milkbottle_ cc_name, "invoke", ["putvalue", milk_bottle, parm]); 
             for (let i = 0; i ＜ chiancodequeryresutl.length; i++) { 
               res.send( chiancodequeryresutl[i].toString( 'utf8' ) ); 
-            } }).catch((err) =＞ { res.send(err); }) }); 
+            } }).catch((err) =＞ { res.send(err); }) 
+        }
+       ); 
 // 客户端查询牛奶的历史 
 app.get('/getmilhistory', 
         function (req, res) { 
@@ -52,7 +63,9 @@ app.get('/getmilhistory',
             var chiancodequeryresutl = yield fabricservice.queryCc(milkbottle_cc_name, "invoke", ["getmilkhistory", milk_bottle, "a"], ) 
             for (let i = 0; i ＜ chiancodequeryresutl.length; i++) { 
               res.send( chiancodequeryresutl[i].toString( 'utf8' ) ); 
-            } }).catch((err) =＞ { res.send(err); }) }); 
+            } }).catch((err) =＞ { res.send(err); }) 
+        }
+       ); 
 // 启动http服务 
 var server = app.listen(3000, function () { 
   var host = server.address().address; 
